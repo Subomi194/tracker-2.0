fdf97355032fd37e53826143bd5d4c2835cb1213
Subomi194 <subomiariyo@gmail.com>
2026-02-05 21:24:31 +0000

    add callback2

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,23 +1,30 @@
 import { createClient } from "@/lib/supabase/server"
+import { type EmailOtpType } from "@supabase/supabase-js"
 import { redirect } from "next/navigation"
 
 export default async function AuthCallback({searchParams,
 }: {
-  searchParams: { code?: string }
+  searchParams: {
+    token_hash?: string
+    type?: EmailOtpType
+  }
 }) {
-  const code = searchParams.code
-
-  if (!code) {
-    redirect('/auth-error')
+  const { token_hash, type } = searchParams
+ 
+  if (!token_hash || !type) {
+    redirect("/auth-error")
   }
 
   const supabase = await createClient()
 
-  const { error } = await supabase.auth.exchangeCodeForSession(code)
+  const { error } = await supabase.auth.verifyOtp({
+    token_hash,
+    type,
+  })
 
   if (error) {
-    redirect('/auth-error')
+    redirect("/auth-error")
   }
 
-  redirect('/profile')
+  redirect("/profile")
 }

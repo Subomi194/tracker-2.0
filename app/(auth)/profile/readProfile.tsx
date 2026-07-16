da58dc52cb9a02289a06d4de195d2d0781afe3cc
Subomi194 <subomiariyo@gmail.com>
2026-07-16 17:18:38 +0100

    fixed deployment error2

--- app/(auth)/profile/readProfile.tsx
+++ app/(auth)/profile/readProfile.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import {createClient} from '@/lib/supabase/server'
-import { Hero } from '@/components'
-import { UserProps } from '@/types/user'
+// import React from 'react'
+// import {createClient} from '@/lib/supabase/server'
+// import { Hero } from '@/components'
+// import { UserProps } from '@/types/user'
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-export default async function readProfile() {
-  const supabase = await createClient()
+// export default async function readProfile() {
+//   const supabase = await createClient()
 
-  const { data: { user } } = await supabase.auth.getUser()
-  console.log("SERVER USER:", user)
-  if (!user) return null
+//   const { data: { user } } = await supabase.auth.getUser()
+//   console.log("SERVER USER:", user)
+//   if (!user) return null
 
-  const { data: profile } = await supabase
-    .from('profiles')
-    .select('name')
-    .eq('id', user.id)
-    .single()
+//   const { data: profile } = await supabase
+//     .from('profiles')
+//     .select('name')
+//     .eq('id', user.id)
+//     .single()
 
-  return <Hero name={profile?.name}
-  email={user.email} />
-}
+//   return <Hero name={profile?.name}
+//   email={user.email} />
+// }
